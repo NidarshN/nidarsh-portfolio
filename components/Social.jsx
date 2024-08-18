1a0b9575a57776fd103c4dee2eea5f0b998c3cc3
Nidarsh N <nidarsh39@gmail.com>
2024-08-18 18:52:21 +0530

    Updated Home page and Navs

---
+++ components/Social.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import {FaGithub, FaLinkedin, FaHackerrank, FaInstagram} from "react-icons/fa";
+
+const socials = [
+    {
+        icon: <FaGithub/>, path: ''
+    },
+    {
+        icon: <FaLinkedin/>, path: ''
+    },
+    {
+        icon: <FaHackerrank/>, path: ''
+    },
+    {
+        icon: <FaInstagram/>, path: ''
+    },
+]
+const Social = ({containerStyles, iconStyles}) => {
+    return(
+        <div className={containerStyles}>
+            {socials.map((item, index)=>{
+                return <Link key={index} href={item.path} className={iconStyles}>
+                    {item.icon}
+                </Link>
+            })}
+        </div>
+    );
+};
+
+export default Social;
---
+++ components/Stats.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+    {
+        num: 2,
+        text: "Years of Experience"
+    },
+    {
+        num: 26,
+        text: "Projects Completed"
+    },
+    {
+        num: 8,
+        text: "Technologies Mastered"
+    },
+    {
+        num: 500,
+        text: "Code Commits"
+    }
+]
+
+const Stats = () => {
+    return (
+        <div>
+            <p>Hello </p>Stats
+        </div>
+    );
+};
+
+export default Stats;
